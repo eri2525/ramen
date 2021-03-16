dddebd15321b494a6ce2597fcdc69166e5238a25
eri2525 <71331326+eri2525@users.noreply.github.com>
2021-03-16 23:28:50 +0900

    seedによるデータの投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  (1..50).each do |i|
+    Shop.create(name: "店舗名#{i}", text: "コメント#{i}")
+  end
+
