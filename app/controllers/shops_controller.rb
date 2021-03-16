ffdf4a3c5918c79d5d0008fe8eef39c1513a369f
eri2525 <71331326+eri2525@users.noreply.github.com>
2021-03-16 21:14:11 +0900

    createアクションによるデータの保存

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -6,4 +6,13 @@ class ShopsController < ApplicationController
   def new
     @shop = Shop.new
   end
+
+  def create
+    Shop.create(shop_params)
+  end
+
+  private
+  def shop_params
+    params.require(:shop).permit(:name, :image, :text)
+  end
 end
