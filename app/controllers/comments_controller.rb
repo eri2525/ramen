023a0a1c2b68b24bdc914cc22ce40fa225fbd369
eri2525 <71331326+eri2525@users.noreply.github.com>
2021-03-18 13:45:05 +0900

    コメントデータの保存

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,22 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      flash[:notice] = 'コメントを投稿しました'
+      redirect_to comment.shop
+    else
+      flash[:comment] = comment
+      flash[:error_messages] = coment.errors.full_messages
+      redirect_back fallback_location: comment.shop
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:shop_id, :name, :comment)
   end
 end
