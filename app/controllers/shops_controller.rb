f1f5edc7b678950ef1e6c2f8e9f7614d8e97411b
eri2525 <71331326+eri2525@users.noreply.github.com>
2021-03-19 00:40:08 +0900

    タグの追加と検索

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,7 +2,8 @@ class ShopsController < ApplicationController
   before_action :set_target_shop, only: [:show, :edit, :update, :destroy]
 
   def index
-    @shops = Shop.page(params[:page]).per(10)
+    @shops = params[:tag_id].present? ? Tag.find(params[:tag_id]).shops : Shop.all
+    @shops = @shops.page(params[:page]).per(10)
   end
 
   def new
@@ -19,7 +20,6 @@ class ShopsController < ApplicationController
         shop: shop,
         error_messages: shop.errors.full_messages
       }
-      
     end
   end
 
@@ -48,10 +48,10 @@ class ShopsController < ApplicationController
 
   private
   def shop_params
-    params.require(:shop).permit(:name, :image, :text, tag_ids: [])
+    params.require(:shop).permit(:name, :image, :text, tag_ids:[])
   end
 
   def set_target_shop
-    @shop = Shop.find(params[:id])
+    @shop = Shop.find(params[:id] )
   end
 end
